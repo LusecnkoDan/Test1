11e1e4a17805769eb12ed449729d10de4fdec813
LusecnkoDan <49034428+LusecnkoDan@users.noreply.github.com>
2019-03-29 00:03:24 +0300

    Update myfunc.c

--- app/myfunc.c
+++ app/myfunc.c
@@ -2,15 +2,15 @@
 int myfunc(int b) {
     return b+4;
 }
-int fibonachi(int num) {
+int fibonachi(int n) {
     int prev = 1;
     int next = 1;
     if (num < 0)
         return 0;
     if (num <= 2)
-        return num;
+        return n;
     int i = 2;
-    while (i < num) {
+    while (i < n) {
         next += prev;
         prev = next - prev;
         i++;
